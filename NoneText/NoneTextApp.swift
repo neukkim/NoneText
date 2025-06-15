83deb9ff6f9d2da67dbb5014def9c33aa84fdeba
sunminhong <akoda5555@gmail.com>
2025-06-15 23:07:17 +0900

    Initial Commit

---
+++ NoneText/NoneTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoneTextApp.swift
+//  NoneText
+//
+//  Created by SunMin Hong on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NoneTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
