53d323a7f33ee3a9f36ae40641ca634120d46049
sunminhong <akoda5555@gmail.com>
2025-06-19 19:11:50 +0900

    세부 Ui 구성중

---
+++ NoneText/ResultView.swift
@@ -0,0 +1,8 @@
+//
+//  ResultView.swift
+//  NoneText
+//
+//  Created by SunMin Hong on 6/19/25.
+//
+
+import Foundation
---
+++ NoneText/TextExtractView.swift
@@ -0,0 +1,8 @@
+//
+//  TextExtractView.swift
+//  NoneText
+//
+//  Created by SunMin Hong on 6/19/25.
+//
+
+import Foundation
